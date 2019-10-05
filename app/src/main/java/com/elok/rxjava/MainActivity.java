55a45c76704d2f1d5bebcd70b97d654860bf1dd7
elokanugrah <33139266+elokanugrah@users.noreply.github.com>
2019-10-05 20:14:34 +0700

    create operator

--- app/src/main/java/com/elok/rxjava/MainActivity.java
+++ app/src/main/java/com/elok/rxjava/MainActivity.java
@@ -10,9 +10,14 @@ import com.elok.rxjava.models.Task;
 import com.elok.rxjava.util.DataSource;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
+import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
@@ -24,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView text;
 
     //vars
-
+    private CompositeDisposable disposable =  new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,17 +38,56 @@ public class MainActivity extends AppCompatActivity {
 
         text = findViewById(R.id.text);
 
+        final Task task = new Task("Walk the dog", false, 3);
+
         Observable<Task> taskObservable = Observable
+                .create(new ObservableOnSubscribe<Task>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<Task> emitter) throws Exception {
+
+                        if (!emitter.isDisposed()) {
+                            emitter.onNext(task);
+                            emitter.onComplete();
+                        }
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Task task) {
+                Log.d(TAG, "onNext: " + task.getDescription());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+        /*Observable<Task> taskObservable = Observable
                 .fromIterable(DataSource.createTasksList())
                 .subscribeOn(Schedulers.io())
                 .filter(new Predicate<Task>() {
                     @Override
                     public boolean test(Task task) throws Exception {
-                        try {
+                        Log.d(TAG, "test: " + Thread.currentThread().getName());
+                        *//*try {
                             Thread.sleep(1000);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
-                        }
+                        }*//*
                         return task.isComplete();
                     }
                 })
@@ -53,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSubscribe(Disposable d) {
                 Log.d(TAG, "onSubscribe: called.");
+                disposable.add(d);
             }
 
             @Override
@@ -71,5 +116,18 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "onComplete: called.");
             }
         });
+
+        disposable.add(taskObservable.subscribe(new Consumer<Task>() {
+            @Override
+            public void accept(Task task) throws Exception {
+
+            }
+        }));*/
     }
+
+    /*@Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposable.clear();
+    }*/
 }
